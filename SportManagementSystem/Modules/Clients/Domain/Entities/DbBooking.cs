1b03dc847ed1fa399c8222ac2c947de505712ad4
Konstantin Lygin (LyginKA) <k.lygin@modulbank.ru>
2026-02-13 18:59:19 +0300

    Почистил проект

--- SportManagementSystem/Modules/Clients/Domain/Entities/DbBooking.cs
+++ SportManagementSystem/Modules/Clients/Domain/Entities/DbBooking.cs
@@ -1,4 +1,4 @@
-using SportManagementSystem.Entities.Enums;
+using SportManagementSystem.Modules.Clients.Domain.Enums;
 using SportManagementSystem.Modules.Scheduling.Domain.Entities;
 using SportManagementSystem.Modules.Users.Domain.Entities;
 
